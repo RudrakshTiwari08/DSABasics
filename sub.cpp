8cc44deacb157759233dc6368d46a14d58d4eaee
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-02-05 15:03:12 +0530

    Add files via upload
    This C++ program performs a simple subtraction operation between two integers provided by the user. The program prompts the user to enter two numbers, calculates the difference, and then displays the result. This example demonstrates basic input/output operations and arithmetic in C++.

---
+++ sub.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int subtraction = 0;
+    int a;
+    int b;
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    subtraction = a-b;
+    cout<<subtraction;
+
+
+
+}
