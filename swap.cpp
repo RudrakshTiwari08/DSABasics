35a109218e08de79e37c96754d31c5d72830f066
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-02-05 14:36:44 +0530

    Add files via upload
    This C++ program demonstrates how to swap the values of two integers using a temporary variable. The program prompts the user to input two integers and then swaps their values before displaying the results. This simple example illustrates basic input/output operations and variable manipulation in C++.

---
+++ swap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout<<"enter a";
+    cin>>a;
+
+    cout<<"enter b";
+    cin>>b;
+
+ 
+
+    
+    c=a;
+    a=b;
+    b=c;
+
+    cout<<"a"<<a;
+    cout<<"b"<<b;
+
+}
