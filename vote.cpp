836614cee82a9f08c4d3028211ea00780dbdf7f5
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-02-05 14:19:35 +0530

    Add files via upload
    This C++ program checks whether a user is eligible to vote based on their age. It prompts the user to enter their age and determines if they meet the minimum voting age requirement. The program is simple and serves as a basic example of conditional statements in C++.

---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a;
+    cout<<"enter the age";
+    cin>>a;
+    if(a>18) { 
+        cout<<"can vote for elections";
+    }
+    else {
+        cout<<"cannot vote for election";
+    }    
+    
+
+}
