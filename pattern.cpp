7ffdcb12646d92b3953f2e8ff7ec486d02a5b1db
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 16:47:19 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that prints a star pattern. This exercise helped me practice using loops and output formatting.

---
+++ pattern.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int i;
+
+//     for (size_t i = 0; i < 5; i++)
+//     {
+//         cout<<"*";
+//     }
+    
+// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout<<"*"<<endl;;
+    }
+    
+}
