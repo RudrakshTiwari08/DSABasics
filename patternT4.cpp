dfce0dec21f172436bebe1ceed43e70705f90bbe
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 16:56:50 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that prints a decreasing right-angled triangle star pattern. This exercise helped me practice using nested loops and conditional statements.

---
+++ patternT4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 4; j >=0 ; j--)
+        {
+            if (i <= j)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+// *****
+// ****
+// ***
+// **
+// *
