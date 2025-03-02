e8867f3b0f2d286fdacf7d556ff05d7809285f0b
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 16:51:36 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that prints an alternating character pattern using nested loops. This exercise helped me practice using loops, conditionals, and output formatting.

---
+++ pattern4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i;
+    int j;
+    for ( i = 0; i < 5 ; i++)
+        {
+            for (size_t j = 0; j < 5; j++)
+            {
+                if (j%2==0)
+                {
+                    cout<<"a";
+
+                }
+                else{
+                    cout<<"b";
+                }
+                
+
+
+            }
+         cout<<endl;
+            
+        }
+        
+    }
+    
