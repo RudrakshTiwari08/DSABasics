ca11689ed17881a4e479ae0f444433b5db654b73
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 17:02:00 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that prints two different star patterns: an inverted right-angled triangle followed by a right-angled triangle. This exercise helped me practice using nested loops and conditional statements.

---
+++ patternT8.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 4; j >= 0; j--)
+        {
+            if (i >= j)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        for (size_t j = 0; j < 5; j++)
+        {
+            if (i <= j)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout<<endl;
+    }
+}
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// *****
+//  ****
+//   ***
+//    **
+//     *
