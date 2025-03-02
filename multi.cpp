e87135df61541101c7151cf8e647d2b15985871d
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 17:14:57 +0530

    Add files via upload
    As a first-year student learning to code, I created a simple C++ program that multiplies two numbers provided by the user. This exercise helped me practice basic input/output operations and arithmetic calculations in C++

---
+++ multi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int multiplication=0;
+    int a;
+    int b;
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    multiplication = a*b;
+    cout<<multiplication;
+    
+
+}
