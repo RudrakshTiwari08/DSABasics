19ca0ffbd78a114ff535021e979610acefca647d
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 13:43:27 +0530

    Add files via upload
    As a first-year student exploring the fundamentals of programming, I wrote a simple C++ program to display a list of roll numbers stored in an array. This exercise helped me practice using arrays and loops, which are essential concepts in programming.

---
+++ arr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int rollno[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (size_t i = 0; i < 9; i++)
+    {
+        cout << rollno[i];
+    }
+
+    return 0;
+}
