1364924abbbda7a1a8777e37c75884dd995554b8
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 16:51:06 +0530

    Add files via upload
    #include <iostream>
    using namespace std;
    int main(){
    
        int i;
        int j;
        for ( i = 0; i < 5 ; i++)
            {
                for (size_t j = 0; j < 5; j++)
                {
                    cout<<i;
    
                }
             cout<<endl;
                
            }
            
        }
    
    
        // 00000
        // 11111
        // 22222
        // 33333
        // 44444

---
+++ pattern3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i;
+    int j;
+    for ( i = 0; i < 5 ; i++)
+        {
+            for (size_t j = 0; j < 5; j++)
+            {
+                cout<<i;
+
+            }
+         cout<<endl;
+            
+        }
+        
+    }
+
+
+    // 00000
+    // 11111
+    // 22222
+    // 33333
+    // 44444
