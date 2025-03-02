fe473a96c76059fe161ee4d766b0f50361b25555
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 13:51:20 +0530

    Add files via upload
    As a first-year student learning to code, I recently created a simple C++ program that allows users to input a series of numbers into an array and then displays those numbers. This exercise helped me practice using arrays, loops, and user input in C++.

---
+++ array2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[8]; // declaration
+
+    for(int i = 0; i < 8; i++)
+    {
+        cout<<"enter the numbers"<<endl;
+       cin>>arr[i]; //user input
+    }
+
+    for (int i = 0; i < 8; i++) // output
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+}
