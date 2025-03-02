9802b9bf3df1a18132f2e36c7391d518241e7061
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 14:01:07 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that calculates the average of three numbers entered by the user. This exercise helped me practice using variables, user input, and basic arithmetic operations.

---
+++ average.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int sum=0;
+    int divide=0;
+    cout<<"enter 1st number";
+    cin>>a;
+    cout<<"enter 2nd number";
+    cin>>b;
+    cout<<"enter 3rd number";
+    cin>>c;
+    sum=a+b+c;
+    divide= sum/2;
+    cout<<"average of the three numbers";
+       cout<<divide;
+    
+
+}
