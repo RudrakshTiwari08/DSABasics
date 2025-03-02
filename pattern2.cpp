bf63ff9d7e7b92894de4c134711f309544826d16
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 16:49:45 +0530

    Add files via upload
    As a first-year student learning to code, I wrote a simple C++ program that prints a number pattern using nested loops. This exercise helped me practice using loops and output formatting.

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i;
+    int j;
+    for ( i = 0; i < 5 ; i++)
+        {
+            for (size_t i = 0; i < 5; i++)
+            {
+                cout<<i;
+
+            }
+         cout<<endl;
+            
+        }
+        
+    }
+    
