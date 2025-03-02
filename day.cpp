2a8fcf5e2605bb1e70b3c5b7693b0209c7e81dca
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 17:10:11 +0530

    Add files via upload
    As a first-year student learning to code, I created a simple C++ program that takes a number input from the user (between 1 and 7) and outputs the corresponding day of the week. This exercise helped me practice using conditional statements and user input handling.

---
+++ day.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"enter the number of the week btw 1-7";
+    cin>>n;
+
+    if (n==1)
+    {
+        cout<<"MONDAY";
+    }
+    if (n==2)
+    {
+        cout<<"TUESDAY";
+    }
+    if (n==3)
+    {
+        cout<<"WEDNESDAY";
+    }
+    if (n==4)
+    {
+        cout<<"THRUSDAY";
+    }
+    if (n==5)
+    {
+        cout<<"FRIDAY";
+    }
+    if(n==6)
+    {
+        cout<<"SATURDAY";
+    }
+    if (n==7)
+    {
+        cout<<"SUNDAY";
+    }
+}
+    
