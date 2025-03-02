0386dd379f101390a2ad106e5dd50b0817a96314
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-02 13:49:09 +0530

    Add files via upload
    As a first-year student learning to code, I recently wrote a C++ program that demonstrates how to use vectors to store and display a sequence of numbers. This exercise helped me understand dynamic arrays and the flexibility that vectors provide in C++.

---
+++ array1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ vector<int>vec;
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    for (int i = 0; i <=n; i++)
+    {
+     vec.push_back(i);
+     cout<<vec[i]<<endl;
+    }
+    
+}
