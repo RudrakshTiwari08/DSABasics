91ded559d080027f7876752a0b8c9532886454f6
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-03-05 20:01:21 +0530

    Add files via upload
    This C++ program solves the problem of dividing a watermelon of weight w kilos into two even-weight parts, ensuring that both parts are positive. The program takes an integer input representing the weight of the watermelon and checks if it can be split into two even numbers greater than zero. If the conditions are met, it outputs "YES"; otherwise, it outputs "NO". This simple yet effective solution demonstrates basic conditional logic and is a great exercise for understanding even numbers and their properties.

---
+++ watermelon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w; 
+    int x; 
+    int t;
+
+    cin >> w; 
+
+    if((w%2==0)&&(w>2)) {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+
+return 0;
+
+}
