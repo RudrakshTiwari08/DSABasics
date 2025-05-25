08ae4e104a12fa1d36f4073e3d7176775fda4b5c
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-05-25 23:20:43 +0530

    Update nearly_lucky.cpp

--- nearly_lucky.cpp
+++ nearly_lucky.cpp
@@ -1,32 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    int count = 0;
+int main() {
+    string n;
+    cin >> n;  // Read input as string
 
-    // Count the lucky digits (4 and 7) in the number
-    while (n > 0)
-    {
-        int digit = n % 10; // Get the last digit
-        if (digit == 4 || digit == 7)
-        {
+    int count = 0;
+    for (int i = 0; i < n.size(); ++i) {
+        char digit = n[i];
+        if (digit == '4' || digit == '7') {
             count++;
         }
-        n /= 10; // Remove the last digit
     }
 
-    // Check if the count of lucky digits is a lucky number
-    if (count == 4 || count == 7)
-    {
+   
+
+    if (count == 4 || count == 7) {
         cout << "YES";
-    }
-    else
-    {
+    } else {
         cout << "NO";
     }
 
     return 0;
 }
+
