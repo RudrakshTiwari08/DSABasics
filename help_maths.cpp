07ba82153fecf4fa6be30e937350a2416efb6db4
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-10-24 13:09:56 +0530

    Add files via upload

---
+++ help_maths.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;        
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<char> vec;
+    for( int i = 0 ; i < n ; i+=2){
+        char c = s[i];
+        // cout<<c<<endl;
+        vec.push_back(c);
+    }
+    sort(vec.begin(), vec.end());
+    for(int i = 0 ; i<vec.size() ; i++){
+        if( i == vec.size() -1){
+            cout<<vec[i];
+        }
+        else{
+        cout<<vec[i]<<"+";
+        }
+    }
+    return 0;
+
+}
